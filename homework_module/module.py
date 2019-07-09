88288daf3c9810f46694b6647414467fca726e89
[leylairoyale] <[leylairoyale@gmail.com]>
2019-07-09 06:51:53 -0500

    week two continued day one, building a basic shopping cart and creating and importing modules.

---
+++ homework_module/module.py
@@ -0,0 +1,11 @@
+def square_footage(length, width):
+    """this function takes 2 arguments,length and width, to find the area of a house."""
+    area = length * width
+    print("The area is {}".format(area))
+
+
+def circum_circle(radius):
+    from math import pi
+    """this function takes 1 variable, radius, to find the circumference of a circle."""
+    circum = 2 * pi * radius
+    print("Sir Cumference is {}, as round as the Round Table.".format(circum))
---
+++ module/module.py
@@ -0,0 +1,3 @@
+full_name = "leyla raoooy"
+def printhello(name):
+    print("hello" + name)
